8e79c1f9ea67997ffa37ccb66c1a83cac6b7c8fe
Taha Koçal <tahakocalgs@gmail.com>
2021-10-13 17:53:39 +0300

    13.10.2021

--- DataAccess/Concrete/EntityFramework/EfCategoryDal.cs
+++ DataAccess/Concrete/EntityFramework/EfCategoryDal.cs
@@ -7,31 +7,8 @@ using System.Linq.Expressions;
 
  namespace DataAccess.Concrete.EntityFramework
 {
-    public class EfCategoryDal : EfEntityRepositoryBase<Product, NorthwindContext>, ICategoryDal
+    public class EfCategoryDal : EfEntityRepositoryBase<Category, NorthwindContext>, ICategoryDal
     {
-        public List<Category> GetAll(Expression<Func<Category, bool>> filter = null)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Category Get(Expression<Func<Category, bool>> filter)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Add(Category entity)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Update(Category entity)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Delete(Category entity)
-        {
-            throw new NotImplementedException();
-        }
+       
     }
 }
