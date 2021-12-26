1ead118c7a6b1edfefb12f531ec0b1dd025e642e
Taha <tahakocalgs@gmail.com>
2021-12-26 18:02:45 +0300

    26.12.2021

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Business.Abstract;
+using Entities.Concrete;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+
+
+        [HttpGet]
+
+        public List<Product> Get()
+        {
+           
+            var result = _productService.GetAll();
+            return result.Data;
+        }
+    }
+}
